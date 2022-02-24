febfc9da5b8517152af66f4f17984faee3121e00
Shanawaz444 <shanawaz0891@gmail.com>
2022-02-24 09:03:36 +0530

    regestered the intro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroPageComponent } from './intro-page/intro-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroPageComponent
   ],
   imports: [
     BrowserModule,
